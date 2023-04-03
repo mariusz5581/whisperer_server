006fc2a42384505c284efc9846e81a5f58fd94fb
mariusz5581 <mariusz5581@gmail.com>
2023-04-03 11:30:09 +0300

    1

--- server.js
+++ server.js
@@ -15,6 +15,24 @@ function readXLSFile(filename) {
   return XLSX.utils.sheet_to_json(sheet);
 }
 
+function readMessages(userFile, recipient) {
+  const workbook = XLSX.readFile(userFile);
+  
+  if (!workbook.SheetNames.includes(recipient)) {
+    // Create a new sheet for the recipient
+    const newSheet = XLSX.utils.json_to_sheet([]);
+    workbook.SheetNames.push(recipient);
+    workbook.Sheets[recipient] = newSheet;
+
+    // Save the updated workbook
+    XLSX.writeFile(workbook, userFile);
+  }
+
+  const sheet = workbook.Sheets[recipient];
+  return XLSX.utils.sheet_to_json(sheet);
+}
+
+
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   const users = readXLSFile('users.xls');
@@ -28,6 +46,20 @@ app.post('/login', (req, res) => {
   } else {
     res.status(401).send('Invalid username or password');
   }
+});
+
+app.get('/messages/:username/:recipient', (req, res) => {
+  const { username, recipient } = req.params;
+  const users = readXLSFile('users.xls');
+  const user = users.find((u) => u.Username === username);
+
+  if (!user) {
+    res.status(404).send('User not found');
+    return;
+  }
+
+  const messages = readMessages(user.MessagesFile, recipient);
+  res.status(200).json(messages);
 });
 
 app.listen(port, () => {
