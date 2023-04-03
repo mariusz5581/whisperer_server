21100135a7f2c41375b3d7ae5d61e886421b0c4e
mariusz5581 <mariusz5581@gmail.com>
2023-04-03 11:53:54 +0300

    1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const XLSX = require('xlsx');
 const fs = require('fs');
-
+const debug = require('debug')('server');
 const app = express();
 const port = 3000;
 
